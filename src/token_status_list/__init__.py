e405b7a1a8316b34d534526bde1e4e6580089108
Daniel Bluhm <dbluhm@pm.me>
2024-06-28 16:46:21 -0400

    feat: initial commit
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ src/token_status_list/__init__.py
@@ -0,0 +1,7 @@
+"""Token Status List.
+
+Python implementation of Token Status List.
+
+This implementation is based on draft 2, found here:
+https://datatracker.ietf.org/doc/html/draft-ietf-oauth-status-list-02
+"""
